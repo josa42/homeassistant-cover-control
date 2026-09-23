ac04c73d66a9cfa3a4c75bcab236dbacadd645e1
Josa Gesell <josa@gesell.me>
2026-09-23 14:42:06 +0200

    chore: bump version to 1.10.0

--- custom_components/cover_control/__init__.py
+++ custom_components/cover_control/__init__.py
@@ -24,7 +24,7 @@ _LOGGER = logging.getLogger(__name__)
 CONFIG_SCHEMA = cv.config_entry_only_config_schema(DOMAIN)
 
 STRATEGY_URL_PATH = "/cover-control/cover-control-dashboard.js"
-STRATEGY_VERSION = "1.8.0"
+STRATEGY_VERSION = "1.10.0"
 
 PLATFORMS: list[Platform] = [
     Platform.BINARY_SENSOR,
